2d55b2c1d0fa87ab790265ed65b67ff2a759f86a
KMV <karl-martin.vogel@tc.de>
2022-07-07 16:21:28 +0200

    Add project files.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace DependencyInjection
+{
+    public class PrintingPress
+    {
+        public static void Main()
+        {
+            //PrintingPress press = new PrintingPress();
+            Page page = new Page(new YourBook());
+            page.add("Auf diese Seite wurde was geschrieben!\n");
+        }
+
+
+        public class Page
+        {
+            IBook book;
+            public Page(IBook book)
+            {
+                this.book = book;
+            }
+
+            public void add(string data)
+            {
+                book.persist(data);
+            }
+        }
+
+        public interface IBook
+        {
+            void persist(string data);
+        }
+
+        public class MyBook : IBook
+        {
+            public void persist(string data)
+            {
+                Console.WriteLine("MyBook has stored data: " + data);
+            }
+        }
+        public class YourBook : IBook
+        {
+            public void persist(string data)
+            {
+                Console.WriteLine("YourBook has stored data: " + data);
+            }
+        }
+    }
+}
